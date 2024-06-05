96305ea2521c9c7c1099195afc0e52f7216a74e1
HenryHuang <henry.2039@gmail.com>
2024-06-05 11:28:16 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as constant from './src/constants/constant';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `${constant.BASE}/`
 })
